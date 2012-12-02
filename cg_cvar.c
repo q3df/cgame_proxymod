9765360bb8c32060dca8ef572698c2144a6f6fa9
hk <hk@sockenbuegeln.de>
2012-12-02 19:18:30 +0100

    cvar sniffing files

---
+++ cg_cvar.c
@@ -0,0 +1,8 @@
+#include "cg_local.h"
+#include "cg_cvar.h"
+
+
+
+int8_t cvar_register( char *name, char *value ) {
+	return 0;
+}
---
+++ cg_cvar.h
@@ -0,0 +1,8 @@
+#ifndef __CG_CVAR_H__
+#define __CG_CVAR_H__
+
+
+
+
+
+#endif // __CG_CVAR_H__
